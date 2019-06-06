238edaf4e20e9edbbca6abb2d03f8c8027fae915
Radu <avadanii@tiscali.it>
2019-06-06 14:10:26 +0300

    used destruction feature

--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -5,16 +5,16 @@ const geocode = (address, callback) => {
     address
   )}.json?access_token=pk.eyJ1IjoiYXZhZGFyYWQiLCJhIjoiY2p3aXZ3NG45MDBmaTN5cDM4YnJsaHB3aCJ9.qsk2y0ExOmhMFROzXJRDNA&limit=1`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to location services!');
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback('Unable to find location. Try another search', undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       });
     }
   });
